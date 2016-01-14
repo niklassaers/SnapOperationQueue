ad36bf1ec766fa82e7f8572413e4579a2018d564
Niklas Saers <niklas@saers.com>
2016-01-14 07:03:49 +0100

    Initial Commit

---
+++ SnapOperationQueue/SnapOperationQueue.h
@@ -0,0 +1,19 @@
+//
+//  SnapOperationQueue.h
+//  SnapOperationQueue
+//
+//  Created by Niklas Saers on 14/01/16.
+//  Copyright © 2016 Snapsale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnapOperationQueue.
+FOUNDATION_EXPORT double SnapOperationQueueVersionNumber;
+
+//! Project version string for SnapOperationQueue.
+FOUNDATION_EXPORT const unsigned char SnapOperationQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnapOperationQueue/PublicHeader.h>
+
+
