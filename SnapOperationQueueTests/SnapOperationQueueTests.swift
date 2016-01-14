2029058b6fe82e41a92290de546db0067e95feec
Niklas Saers <niklas@saers.com>
2016-01-14 14:33:02 +0100

    Write the first failing test
    or rather, scenario....

--- SnapOperationQueueTests/SnapOperationQueueTests.swift
+++ SnapOperationQueueTests/SnapOperationQueueTests.swift
@@ -1,36 +1,58 @@
-//
-//  SnapOperationQueueTests.swift
-//  SnapOperationQueueTests
-//
-//  Created by Niklas Saers on 14/01/16.
-//  Copyright © 2016 Snapsale. All rights reserved.
-//
-
 import XCTest
 @testable import SnapOperationQueue
+import PSOperations
 
 class SnapOperationQueueTests: XCTestCase {
+
+    var opQueue : SnapOperationQueue!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        opQueue = SnapOperationQueue()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        opQueue = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testScenario() {
+
+        let expectedResult = ["C", "E", "G", "H", "A", "B", "F", "D"]
+        var actualResult : [String] = []
+        
+        let theSpeedyGroupId = "theSpeedyGroup"
+        let thePromptGroupId = "thePromptGroup"
+        let theLazyGroupId = "theLazyGroup"
+        let theSlooowGroupId = "theSlooowGroup"
+        
+        let operationA = BlockOperation { _ in delay(0.1) { actualResult.append("A") } }
+        let operationB = BlockOperation { _ in delay(0.3) { actualResult.append("B") } }
+        let operationC = BlockOperation { _ in delay(0.2) { actualResult.append("C") } }
+        let operationD = BlockOperation { _ in delay(0.1) { actualResult.append("D") } }
+        let operationE = BlockOperation { _ in delay(0.3) { actualResult.append("E") } }
+        let operationF = BlockOperation { _ in delay(0.2) { actualResult.append("F") } }
+        let operationG = BlockOperation { _ in delay(0.1) { actualResult.append("G") } }
+        let operationH = BlockOperation { _ in delay(0.3) { actualResult.append("H") } }
+        
+//        opQueue.suspended
+        opQueue.addOperation(operationA, identifier: randomId(), groupIdentifier: theLazyGroupId, priority: .Normal)
+        opQueue.addOperation(operationB, identifier: randomId(), groupIdentifier: theLazyGroupId, priority: .High)
+        opQueue.addOperation(operationC, identifier: randomId(), groupIdentifier: theSpeedyGroupId, priority: .Normal)
+        opQueue.addOperation(operationD, identifier: randomId(), groupIdentifier: theSlooowGroupId, priority: .High)
+        opQueue.addOperation(operationE, identifier: randomId(), groupIdentifier: thePromptGroupId, priority: .Normal)
+        opQueue.addOperation(operationF, identifier: randomId(), groupIdentifier: theLazyGroupId, priority: .High)
+        opQueue.addOperation(operationG, identifier: randomId(), groupIdentifier: thePromptGroupId, priority: .Normal)
+        opQueue.addOperation(operationH, identifier: randomId(), groupIdentifier: thePromptGroupId, priority: .High)
+        
+        opQueue.waitUntilAllOperationsAreFinished()
+        XCTAssertEqual(expectedResult, actualResult, "Two arrays should be identical")
+        
     }
     
 }
+
+
+
+
