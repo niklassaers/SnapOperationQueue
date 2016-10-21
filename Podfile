e753eada65d185513a55bfa6b043047fa2acd833
Niklas Saers <niklas@saers.com>
2016-10-21 13:08:52 +0200

    Swift 3 update

--- Podfile
+++ Podfile
@@ -2,17 +2,18 @@ source 'https://github.com/CocoaPods/Specs.git'
 
 use_frameworks!
 
-platform :ios, '8.0'
+platform :ios, '9.0'
 
 inhibit_all_warnings!
 
 target 'SnapOperationQueue' do
-  pod 'PSOperations'
+  pod 'PSOperations', :git => "https://github.com/pluralsight/PSOperations.git", :branch => "swift3"
 end
 
 target 'Unit tests' do
-  pod 'PSOperations'
+  pod 'PSOperations', :git => "https://github.com/pluralsight/PSOperations.git", :branch => "swift3"
 end
 
 target 'Example' do
+  pod 'PSOperations', :git => "https://github.com/pluralsight/PSOperations.git", :branch => "swift3"
 end
--- SnapOperationQueueTests/SnapOperationQueueTests.swift
+++ SnapOperationQueueTests/SnapOperationQueueTests.swift
@@ -4,13 +4,13 @@ import PSOperations
 
 class SnapOperationQueueTests: XCTestCase {
 
-    var opQueue : protocol<SnapOperationQueueProtocol, NSOperationQueueProtocol>!
+    var opQueue : (SnapOperationQueueProtocol & NSOperationQueueProtocol)?
     
     override func setUp() {
         super.setUp()
         
         opQueue = SnapOperationQueue()
-        opQueue.maxConcurrentOperationCount = 1
+        opQueue?.maxConcurrentOperationCount = 1
     }
     
     override func tearDown() {
@@ -32,38 +32,43 @@ class SnapOperationQueueTests: XCTestCase {
         let theLazyGroupId = "theLazyGroup"
         let theSlooowGroupId = "theSlooowGroup"
         
-        let operationA = BlockOperation { _ in actualResult.append("A"); self.rest() }
-        let operationB = BlockOperation { _ in actualResult.append("B"); self.rest() }
-        let operationC = BlockOperation { _ in actualResult.append("C"); self.rest() }
-        let operationD = BlockOperation { _ in actualResult.append("D"); self.rest() }
-        let operationE = BlockOperation { _ in actualResult.append("E"); self.rest() }
-        let operationF = BlockOperation { _ in actualResult.append("F"); self.rest() }
-        let operationG = BlockOperation { _ in actualResult.append("G"); self.rest() }
-        let operationH = BlockOperation { _ in actualResult.append("H"); self.rest() }
+        let operationA = PSOperations.BlockOperation { _ in actualResult.append("A"); self.rest() }
+        let operationB = PSOperations.BlockOperation { _ in actualResult.append("B"); self.rest() }
+        let operationC = PSOperations.BlockOperation { _ in actualResult.append("C"); self.rest() }
+        let operationD = PSOperations.BlockOperation { _ in actualResult.append("D"); self.rest() }
+        let operationE = PSOperations.BlockOperation { _ in actualResult.append("E"); self.rest() }
+        let operationF = PSOperations.BlockOperation { _ in actualResult.append("F"); self.rest() }
+        let operationG = PSOperations.BlockOperation { _ in actualResult.append("G"); self.rest() }
+        let operationH = PSOperations.BlockOperation { _ in actualResult.append("H"); self.rest() }
         
-        opQueue.addOperation(operationA, identifier: "A", groupIdentifier: theLazyGroupId, priority: .Normal)
-        opQueue.addOperation(operationB, identifier: "B", groupIdentifier: theLazyGroupId, priority: .High)
-        opQueue.addOperation(operationC, identifier: "C", groupIdentifier: theSpeedyGroupId, priority: .Normal)
-        opQueue.addOperation(operationD, identifier: "D", groupIdentifier: theSlooowGroupId, priority: .High)
-        opQueue.addOperation(operationE, identifier: "E", groupIdentifier: thePromptGroupId, priority: .Normal)
-        opQueue.addOperation(operationF, identifier: "F", groupIdentifier: theLazyGroupId, priority: .High)
-        opQueue.addOperation(operationG, identifier: "G", groupIdentifier: thePromptGroupId, priority: .Normal)
-        opQueue.addOperation(operationH, identifier: "H", groupIdentifier: thePromptGroupId, priority: .High)
+        guard let opQueue = opQueue else {
+            XCTFail()
+            return
+        }
+        
+        let _ = opQueue.addOperation(operationA, identifier: "A", groupIdentifier: theLazyGroupId, priority: .normal)
+        let _ = opQueue.addOperation(operationB, identifier: "B", groupIdentifier: theLazyGroupId, priority: .high)
+        let _ = opQueue.addOperation(operationC, identifier: "C", groupIdentifier: theSpeedyGroupId, priority: .normal)
+        let _ = opQueue.addOperation(operationD, identifier: "D", groupIdentifier: theSlooowGroupId, priority: .high)
+        let _ = opQueue.addOperation(operationE, identifier: "E", groupIdentifier: thePromptGroupId, priority: .normal)
+        let _ = opQueue.addOperation(operationF, identifier: "F", groupIdentifier: theLazyGroupId, priority: .high)
+        let _ = opQueue.addOperation(operationG, identifier: "G", groupIdentifier: thePromptGroupId, priority: .normal)
+        let _ = opQueue.addOperation(operationH, identifier: "H", groupIdentifier: thePromptGroupId, priority: .high)
         
         opQueue.setGroupPriorityToHighRestToNormal(thePromptGroupId)
-        opQueue.setGroupPriorityTo(.Highest, groupIdentifier: theSpeedyGroupId)
-        opQueue.setGroupPriorityTo(.Low, groupIdentifier: theSlooowGroupId)
+        opQueue.setGroupPriorityTo(.highest, groupIdentifier: theSpeedyGroupId)
+        opQueue.setGroupPriorityTo(.low, groupIdentifier: theSlooowGroupId)
 
-        let expectation = self.expectationWithDescription("block")
-        let finalOp = BlockOperation { _ in
+        let expectation = self.expectation(description: "block")
+        let finalOp = PSOperations.BlockOperation { _ in
             expectation.fulfill()
         }
         for op in [operationA, operationB, operationC, operationD, operationE, operationF, operationG, operationH] {
             finalOp.addDependency(op)
         }
-        opQueue.addOperation(finalOp, identifier: "Final", groupIdentifier: "Final", priority: .Highest)
+        let _ = opQueue.addOperation(finalOp, identifier: "Final", groupIdentifier: "Final", priority: .highest)
         
-        waitForExpectationsWithTimeout(1.5, handler: nil)
+        waitForExpectations(timeout: 1.5, handler: nil)
         XCTAssertEqual(expectedResult, actualResult, "Two arrays should be identical")
         
     }
